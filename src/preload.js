a1f76c041d53ae20b723b03c999a88f2123f2494
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-07-03 12:14:36 +0200

    Dynamic titles + notification number

--- src/preload.js
+++ src/preload.js
@@ -2,106 +2,29 @@
 // It has the same sandbox as a Chrome extension.
 const customTitlebar = require('custom-electron-titlebar');
 const {remote} = require('electron')
-const log = require('electron-log')
+const log = require('electron-log').scope('Main')
 const devMode = remote.require('./checkDevMode.js')
-const openAboutWindow = require('about-window').default
+
+var titleObj = {
+    baseTitle: "Android Messages",
+    devMode: "👨‍💻",
+    separator: " - ",
+    contactName: ""
+}
+var titlebar
 
 document.addEventListener('DOMContentLoaded', () => {
-  // It does not make sense to use the custom titlebar on macOS where
-  // it only tries to simulate what we get with the normal behavior anyway.
-  if (process.platform != 'darwin') {
-
-    // add a menu
-    const menu = new remote.Menu();
-
-    menu.append(new remote.MenuItem({
-        label: 'File',
-        submenu: [{
-            label: 'About',
-            click(){
-              log.verbose("About called")
-              var aboutWindow = openAboutWindow({
-                icon_path: `${__dirname}/libs/icon.png`,
-                product_name: 'Android Messages',
-                description: `The unofficial but open-source desktop app for Android Messaging`,
-                homepage: 'https://github.com/GreepTheSheep/android-messages-desktop',
-                license: 'Apache-2.0',
-                use_version_info: true,
-                adjust_window_size: false,
-                use_inner_html: true,
-                bug_report_url: 'https://github.com/GreepTheSheep/android-messages-desktop/issues',
-                bug_link_text: '🐛 Found bug?',
-                open_devtools: false,
-                win_options: {
-                  show: false,
-                  maximizable: false,
-                  resizable: false,
-                  minimizable: false,
-                  alwaysOnTop: true,
-                  parent: remote.getCurrentWindow()
-                }
-              });
-              aboutWindow.setTitle('About Android Messages')
-              aboutWindow.on('ready-to-show', () =>{
-                aboutWindow.show()
-              })
-            }
-          },
-          {
-            label: 'Check for Updates',
-            click(){
-                remote.require('./autoUpdater')(false, remote.getCurrentWindow().webContents, undefined)
-            }
-          },
-          {
-            type: 'separator'
-          },
-          {
-            label: 'Exit',
-            click(){
-                var confirm = remote.dialog.showMessageBoxSync(remote.getCurrentWindow(), {
-                    message: "Are you sure to exit? You will not receive any notifications while it is closed",
-                    type: "warning",
-                    buttons: [
-                        "Yes",
-                        "No"
-                    ],
-                    title: "Android Messages",
-                    cancelId: 1
-                })
-                if (confirm == 0){
-                    window.close()
-                }
-            }
-          }]
-      }));
-
-    if (devMode) {
-      menu.append(new remote.MenuItem({
-        label: 'Developer Mode',
-        submenu: [{
-          label: 'Toggle Developer Tools',
-          accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',	
-          click (item, focusedWindow) {
-            if (focusedWindow) remote.getCurrentWebContents().toggleDevTools()
-          }	
-        },
-        {
-          label: 'Open Log Folder',
-          click(){
-            remote.shell.showItemInFolder(log.transports.file.getFile().path)
-          }
-        }]
-      }));
+    if (process.platform != 'darwin') {
+        titlebar = new customTitlebar.Titlebar({
+            backgroundColor: customTitlebar.Color.fromHex('#1A73E8'),
+            icon: 'https://ssl.gstatic.com/images/branding/product/2x/messages_96dp.png',
+            menu: require('./menu')(remote, log, devMode)
+        });
+        var title = []
+        if (devMode) title.push(titleObj.devMode)
+        title.push(titleObj.baseTitle)
+        titlebar.updateTitle(title.join(' '));
     }
-
-    const titlebar = new customTitlebar.Titlebar({
-        backgroundColor: customTitlebar.Color.fromHex('#1A73E8'),
-        icon: 'https://ssl.gstatic.com/images/branding/product/2x/messages_96dp.png',
-        menu
-    });
-    titlebar.updateTitle(`${devMode?'👨‍💻 ':''}Android Messages`);
-  }
 })
 
 document.addEventListener('readystatechange', () => {
@@ -131,5 +54,32 @@ document.addEventListener('readystatechange', () => {
   }
 });
 
-/*global document,window*/
+const observer = new MutationObserver((mutationsList)=>{
+    for(const mutation of mutationsList) {
+        if (mutation.target.nodeName == 'H2'){
+            titleObj.contactName = mutation.target.textContent
+            var title = []
+            if (devMode) title.push(titleObj.devMode)
+            title.push(titleObj.contactName)
+            title.push(titleObj.baseTitle)
+            titlebar.updateTitle(title.join(titleObj.separator));
+        }
+
+        if(mutation.target.nodeName == "TITLE" && !mutation.target.textContent.endsWith(titleObj.baseTitle)){
+            var notifNumber = mutation.target.textContent.substring(0,mutation.target.textContent.indexOf(')')+1)
+            if (notifNumber != ""){
+                titleObj.contactName = notifNumber + " " + titleObj.contactName
+                // eslint-disable-next-line no-redeclare
+                var title = []
+                if (devMode) title.push(titleObj.devMode)
+                title.push(titleObj.contactName)
+                title.push(titleObj.baseTitle)
+                titlebar.updateTitle(title.join(titleObj.separator));
+            }
+        }
+    }
+});
+observer.observe(document, {attributes: true, childList: true, subtree: true});
+
+/*global document,MutationObserver*/
 /*eslint no-undef: "error"*/
